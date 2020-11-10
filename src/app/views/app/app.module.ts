eaa8f6e4b13ba1e4173cb527b8817590ebac854b
jastidev <sujaneshdev@gmail.com>
2020-11-10 12:56:54 -0500

    workstation cleanup

--- src/app/views/app/app.module.ts
+++ src/app/views/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BlankPageComponent } from './blank-page/blank-page.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { LayoutContainersModule } from 'src/app/containers/layout/layout.containers.module';
 
-
 @NgModule({
-  declarations: [BlankPageComponent, AppComponent],
+  declarations: [AppComponent],
   imports: [
     CommonModule,
     AppRoutingModule,
     SharedModule,
-    LayoutContainersModule
-  ]
+    LayoutContainersModule,
+  ],
 })
-export class AppModule { }
-
+export class AppModule {}
