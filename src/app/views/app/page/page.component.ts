eba2a5067ed8b3ed070faaf1c3527080581d2252
jastidev <sujaneshdev@gmail.com>
2020-12-17 20:34:21 -0500

    page layering

--- src/app/views/app/page/page.component.ts
+++ src/app/views/app/page/page.component.ts
@@ -10,6 +10,10 @@ import {
 import { DesignService } from 'src/app/services/design.service';
 import { Item, Page } from 'src/app/models/models';
 import { Colors } from 'src/app/constants/colors.service';
+import { ItemType } from 'src/app/models/enums';
+
+import * as CSS from 'csstype';
+
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
@@ -18,6 +22,8 @@ import { Colors } from 'src/app/constants/colors.service';
 export class PageComponent implements OnInit, AfterViewInit {
   @Input() page: Page;
   @Input() pageId: number;
+
+  ItemType = ItemType;
 
   constructor(
     public ds: DesignService,
@@ -59,7 +65,50 @@ export class PageComponent implements OnInit, AfterViewInit {
     alert('aa');
   }
 
-  strTransform(item: Item) {
-    return `translate(${item.x}px, ${item.y}px) rotate(${item.rotate}deg)`;
+  styleCard(): CSS.Properties {
+    return {
+      borderRadius: '0%',
+      overflow: 'visible',
+      width: this.ds.page_vw + 'px',
+      height: this.ds.page_vh + 'px',
+      boxShadow: this.pageId == this.ds.thePageId ? '' : 'none'
+    };
   }
+
+  styleCardInside(): CSS.Properties {
+    return {
+      transformOrigin: 'top left',
+      transform: `scale(${this.ds.zoomValue / 100})`,
+      width: this.ds.pageW() + 'px',
+      height: this.ds.pageH() + 'px'
+    };
+  }
+
+
+  styleItem(item: Item)  : CSS.Properties {
+    if(item.type===ItemType.image) return {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: item.w + 'px',
+      height: item.h + 'px',
+      transform: `translate(${item.x}px, ${item.y}px) rotate(${item.rotate}deg)`,
+      WebkitTransform: `${item.flipX?'scaleX(-1)':''} + ${item.flipY?' scaleY(-1)':''}`,
+      border: 'none',
+      filter: item.filter,
+      WebkitFilter: item.filter,
+      clipPath: item.clipStyle,
+    };
+
+    if (item.type === ItemType.text) return {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: item.w + 'px',
+      height: 'auto',
+      transform: `translate(${item.x}px, ${item.y}px)`
+    };
+  }
+
+
 }
