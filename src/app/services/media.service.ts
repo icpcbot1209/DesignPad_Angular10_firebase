7e6948e8aa8ddf001e1fff35605a2b702b9cd0f4
Sujanesh Jasti <sujaneshdev@gmail.com>
2021-03-03 10:40:58 +0800

    music feature ( complete )

--- src/app/services/media.service.ts
+++ src/app/services/media.service.ts
@@ -7,7 +7,10 @@ import { DesignService } from './design.service';
 })
 export class MediaService {
   audio = new Audio();
+  volume;
   isPlayMusic: boolean = true;
+  isSoundContent: boolean = false;
+  isMute: boolean = false;
   playMusicProgressTimer;
   currentTime = 0;
   angel: number = 0;
@@ -18,6 +21,12 @@ export class MediaService {
   addMusic() {
     this.audio.src = this.selectedMusic.downloadURL;
 
+    setTimeout(() => {
+      let ele = document.querySelector('.rotateIcon').firstChild as HTMLElement;
+      ele.style.transform = 'rotate(0deg)';
+      ele = document.querySelector('#progress') as HTMLElement;
+      ele.style.width = '0%';
+    });
     this.setDefault();
   }
 
@@ -38,6 +47,8 @@ export class MediaService {
 
     this.audio.load();
     this.audio.currentTime = this.currentTime;
+    this.volume = this.audio.volume * 100;
+
     this.audio.play();
 
     this.playMusicProgress();
@@ -49,32 +60,39 @@ export class MediaService {
 
     let ele = document.querySelector('.rotateIcon').firstChild as HTMLElement;
     ele.style.transform = 'rotate(0deg)';
+    this.angel = 0;
   }
 
   playMusicProgress() {
     let rotateEle = document.querySelector('.rotateIcon').firstChild as HTMLElement;
-    let progressEle = document.querySelector('#progress') as HTMLElement;
 
-    console.log(this.audio.currentTime);
     this.playMusicProgressTimer = setInterval(() => {
       this.angel++;
       if (this.angel == 360) this.angel = 0;
       rotateEle.style.transform = 'rotate(' + this.angel + 'deg)';
 
       this.currentTime = this.audio.currentTime;
-      progressEle.style.width = (this.audio.currentTime / this.duration) * 100 + '%';
+      if (document.querySelector('#progress'))
+        (document.querySelector('#progress') as HTMLElement).style.width = (this.audio.currentTime / this.duration) * 100 + '%';
 
       if (this.audio.currentTime >= this.duration) {
         this.setDefault();
       }
+      if (this.isMute) this.audio.volume = 0;
+      else this.audio.volume = this.volume / 100;
     }, 10);
   }
 
   deleteMusic() {
+    let ele = document.querySelector('.rotateIcon').firstChild as HTMLElement;
+    ele.style.transform = 'rotate(0deg)';
+    ele = document.querySelector('#progress') as HTMLElement;
+    ele.style.width = '0%';
+
+    this.setDefault();
     this.selectedMusic = null;
     this.ds.status = ItemStatus.none;
     this.audio.pause();
-    this.setDefault();
   }
 
   setDefault() {
@@ -83,26 +101,18 @@ export class MediaService {
     this.angel = 0;
 
     clearInterval(this.playMusicProgressTimer);
-
-    setTimeout(() => {
-      let ele = document.querySelector('.rotateIcon').firstChild as HTMLElement;
-      ele.style.transform = 'rotate(0deg)';
-      ele = document.querySelector('#progress') as HTMLElement;
-      ele.style.width = '0%';
-    });
   }
 
   setMusicPosition(pos) {
     if (!this.isPlayMusic) {
-      this.stopMusic();
+      this.audio.pause();
+      clearInterval(this.playMusicProgressTimer);
     }
 
     this.currentTime = (this.selectedMusic.duration * Number.parseFloat(pos)) / 100;
     this.audio.currentTime = this.currentTime;
-    console.log(this.selectedMusic.duration, pos, this.currentTime);
 
     this.isPlayMusic = false;
     this.playMusic();
-    // this.audio.play();
   }
 }
--- src/app/views/app/toolbar/music-toolbar/music-toolbar.component.ts
+++ src/app/views/app/toolbar/music-toolbar/music-toolbar.component.ts
@@ -13,15 +13,13 @@ export class MusicToolbarComponent implements OnInit {
   ngOnInit(): void {}
 
   ngAfterViewInit(): void {
-    // document.getElementById('progress_bar_box').addEventListener('mousedown', function (e) {
-    //   let x = e.pageX - this.offsetLeft;
-    //   let clickedValue = (x / this.offsetWidth) * 100;
-    //   (document.querySelector('#progress') as HTMLElement).style.width = clickedValue + '%';
-    // });
+    (document.querySelector('#progress') as HTMLElement).style.width = (this.media.audio.currentTime / this.media.duration) * 100 + '%';
+    (document.querySelector('.rotateIcon') as HTMLElement).style.border = '2px solid #00c4cc';
   }
 
   ngOnDestroy(): void {
     (document.querySelector('.rotateIcon') as HTMLElement).style.border = '';
+    this.media.isSoundContent = false;
   }
 
   togglePlayButton() {
@@ -42,5 +40,26 @@ export class MusicToolbarComponent implements OnInit {
     let x = e.pageX - ele.offsetLeft;
     let clickedValue = (x / ele.offsetWidth) * 100;
     this.media.setMusicPosition(clickedValue);
+  }
+
+  controlVolume() {
+    this.media.isSoundContent = !this.media.isSoundContent;
+  }
+
+  changeSlide(event) {
+    this.media.volume = event.value;
+
+    if (this.media.volume == 0) this.media.isMute = true;
+    else if (event.value > 0 && this.media.isMute) this.media.isMute = false;
+  }
+
+  inputVolumeValue(event) {
+    if (event.target.value > 100) {
+      this.media.volume = 100;
+    }
+    this.media.volume = event.target.value;
+
+    if (this.media.volume == 0) this.media.isMute = true;
+    else if (this.media.volume > 0 && !this.media.isMute) this.media.isMute = false;
   }
 }
